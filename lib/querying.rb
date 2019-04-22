2af3a95b660dfc4fa21687a2fe5f9ca077d2c924
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-04-22 18:31:26 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,11 +1,11 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT Books.title, Books.year FROM books INNER JOIN series ON Books.series_id = Series.id WHERE Series.id = 1 ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  # "SELECT Characters.name, MAX(LENGTH(Characters.motto)) FROM characters"
+  "SELECT Characters.name, Characters.motto, MAX(LENGTH(Characters.motto)) FROM characters GROUP BY Characters.name"
 end
-
 
 def select_value_and_count_of_most_prolific_species
   "Write your SQL query here"
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT Characters.name, Books.title FROM characters LEFT JOIN books ON Characters.series_id = Books.series_id GROUP BY Characters.name, COUNT(books.title);"
 end
